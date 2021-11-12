56250c674ac209ae8c13a3f8be407a02b1c8301c
Anh <anhphanvt2705@gmail.com>
2021-11-12 13:48:09 -0500

    Progress 54/100

---
+++ OOP/Function/lambda_function.py
@@ -0,0 +1,27 @@
+# Lambda function = function written in 1 line using labda keyword
+#                   accepts any number of arguments, but only has one expression
+#                   (think of it as a shortcut of function)
+#                   (use full if needed for a short period of time, thow-away)
+# Syntax: function_name = lambda parameters:expression
+
+#Normal
+def double(x):
+    return x*2
+
+print(double(5))
+
+#Lambda function
+double_lambda = lambda x:x*2
+print(double(5))
+
+multiply = lambda x,y : x*y
+print(multiply(5,6))
+
+add = lambda x,y,z : x+y+z
+print(add(1, 2, 3))
+
+full_name = lambda first_name, last_name : (first_name + " " + last_name)
+print("Anh", "Phan")
+
+age_check = lambda age : True if age >= 18 else False
+print(age_check(18))
